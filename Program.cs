be7126dc8b230a9e72529512805223e6fa512c95
tongjunjiang <tg626@qq.com>
2017-03-04 12:53:12 +0800

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
